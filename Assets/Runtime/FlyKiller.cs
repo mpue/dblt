9bfde0307e6484bba1c3b66eacae7f0bac889b9f
Matthias Pueski <matthias@pueski.de>
2024-01-03 15:39:11 +0100

    Flies falling

--- Assets/Runtime/FlyKiller.cs
+++ Assets/Runtime/FlyKiller.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Burst.CompilerServices;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -23,9 +24,8 @@ public class FlyKiller : MonoBehaviour
 
         klatsche.transform.position = mousePos2D + klatscheOffset;
 
-        if (Input.GetKeyDown(KeyCode.Space) || (Input.touchCount > 0))
+        if (Input.touchCount > 0)
         {
-
             if (Input.touches[0].tapCount == 1)
             {
                 if (Input.GetTouch(0).phase == TouchPhase.Began)
@@ -36,15 +36,12 @@ public class FlyKiller : MonoBehaviour
                     KillFlyUnderMouse();
                     klatsche.GetComponent<BoxCollider2D>().enabled = true;
                 }
-
                 else if ((Input.GetTouch(0).phase == TouchPhase.Ended))
                 {
-
                     klatsche.GetComponent<SpriteRenderer>().enabled = false;
                     klatsche.transform.localScale *= 1.5f;
                     klatsche.GetComponent<BoxCollider2D>().enabled = false;
                 }
-
             }
         }
     }
@@ -58,25 +55,18 @@ public class FlyKiller : MonoBehaviour
         RaycastHit2D hit = Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Camera.main.transform.forward);
         if (hit && hit.collider.gameObject.GetComponent<CurvedPathMover>() != null)
         {
-            //hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite = deadSprite;
             hit.collider.gameObject.GetComponent<CurvedPathMover>().enabled = false;
+            StartCoroutine(FlyDie(hit.collider.gameObject));
         }
 
-
-        //if (hit.collider != null && hit.collider.gameObject.GetComponent<CurvedPathMover>() != null) 
-        //{
-        //    hit.collider.gameObject.GetComponent<SpriteRenderer>().sprite = deadSprite;
-        //    hit.collider.gameObject.GetComponent<CurvedPathMover>().enabled = false;
-
-        //}
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private IEnumerator FlyDie(GameObject gameObject)
     {
-        if (collision.collider.gameObject.GetComponent<CurvedPathMover>() != null)
-        {
-            //collision.collider.gameObject.GetComponent<SpriteRenderer>().sprite = deadSprite;
-            collision.collider.gameObject.GetComponent<CurvedPathMover>().enabled = false;
-        }
+        yield return new WaitForSeconds(1);
+        gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        Destroy(gameObject, 3f);
     }
+
 }
