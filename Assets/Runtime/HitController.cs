f0b646007c7c12ec661d6187223e783f508914c6
MartinDean <marti22go@yahoo.de>
2024-01-07 01:59:22 +0100

    Fixed offset and collision

--- Assets/Runtime/HitController.cs
+++ Assets/Runtime/HitController.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
 using UnityEngine;
 
 public class HitController : MonoBehaviour
 {
+    public float falltime = 1;
 
+    void OnCollisionEnter2D(Collision2D collision)
+    { 
+        Debug.Log("Collision!");
 
+        //Check kind of Collision
+        if (collision.gameObject.CompareTag("Swatter"))
+        {
+            Debug.Log("Collision with Swatter!");
+            if (GetComponent<CurvedPathMover>()) 
+            {
+                gameObject.GetComponent<CurvedPathMover>().enabled = false;
+            }
+            else if (GetComponent<BeeMover>())
+            {
+                gameObject.GetComponent<BeeMover>().enabled = false;
+            }
+            else if (GetComponent<BumblebeeSpawner>())
+            {
+                gameObject.GetComponent<BumblebeeSpawner>().enabled = false;               
+            }
+            StartCoroutine(Hit(falltime));
+        }         
+    }
+
+    private IEnumerator Hit(float value)
+    {
+        if (gameObject != null)
+        {
+            yield return new WaitForSeconds(value);
+            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+            Destroy(gameObject, 3f);
+        }
+    }
 }
--- Assets/Runtime/Swatter.cs
+++ Assets/Runtime/Swatter.cs
@@ -28,61 +28,19 @@ public class Swatter : MonoBehaviour
             if (Input.touches[0].tapCount == 1)
             {
                 if (Input.GetTouch(0).phase == TouchPhase.Began)
-                {
+                {                    
                     flyswatter.GetComponent<SpriteRenderer>().enabled = true;
                     flyswatterAudioSource.Play();
-                    flyswatter.transform.localScale /= 1.5f;
-                    KillFlyUnderMouse();
+                    //flyswatter.transform.localScale /= 1.5f;                   
                     flyswatter.GetComponent<BoxCollider2D>().enabled = true;
                 }
                 else if ((Input.GetTouch(0).phase == TouchPhase.Ended))
                 {
                     flyswatter.GetComponent<SpriteRenderer>().enabled = false;
-                    flyswatter.transform.localScale *= 1.5f;
+                    //flyswatter.transform.localScale *= 1.5f;
                     flyswatter.GetComponent<BoxCollider2D>().enabled = false;
                 }
             }
         }
-    }
-
-    void KillFlyUnderMouse()
-    {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-
-        flyswatter.transform.position = mousePos2D + flyswatterOffset;
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Camera.main.transform.forward);
-        if (hit && hit.collider.gameObject.GetComponent<CurvedPathMover>() != null)
-        {
-            hit.collider.gameObject.GetComponent<CurvedPathMover>().enabled = false;
-            StartCoroutine(FlyHit(hit.collider.gameObject));
-        }
-        if (hit && hit.collider.gameObject.GetComponent<BeeMover>() != null)
-        {
-            hit.collider.gameObject.GetComponent<BeeMover>().enabled = false;
-            StartCoroutine(BeeHit(hit.collider.gameObject));
-        }
-
-    }
-
-    private IEnumerator FlyHit(GameObject gameObject)
-    {
-        if (gameObject != null)
-        {
-            yield return new WaitForSeconds(1);
-            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-            Destroy(gameObject, 3f);
-        }
-    }
-
-    private IEnumerator BeeHit(GameObject gameObject)
-    {
-        if (gameObject != null)
-        {
-            yield return new WaitForSeconds(1);
-            gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-            Destroy(gameObject, 3f);
-            GameController.beesHit = GameController.beesHit + 1;
-        }
-    }
+    } 
 }
